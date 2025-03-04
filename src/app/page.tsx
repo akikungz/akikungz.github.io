f5fa51b306dbd1fb6a01e654fca01cf57c06bbae
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-03-04 17:31:05 +0700

    Update project grid layout to support four columns on larger screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
         <h2 className="text-2xl font-bold">
           My Projects
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
           { 
             projects
               .filter(({ publish }) => publish)
