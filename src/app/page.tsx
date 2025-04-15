881c6b7ccf875ee541fd8fd079351d10c304b2ff
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-04-15 14:09:42 +0700

    fix: edit name position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,19 +13,24 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col-reverse items-center justify-center md:flex-row md:justify-start lg:justify-center gap-4 p-4">
-        <div className={_class(
-          "mt-4 px-4 max-w-[720px] col-span-2 h-full gap-4",
-          "flex flex-col justify-center items-center md:items-start"
-        )}>
-          <h1 className="text-3xl font-bold">
+        <div
+          className={_class(
+            "mt-4 px-4 max-w-[720px] col-span-2 h-full gap-4",
+            "flex flex-col justify-center items-center md:items-start",
+          )}
+        >
+          <h1 className="text-3xl font-bold text-center">
             Thitipong Tapianthong
           </h1>
           <p className="text-center md:text-left">
-            Hello there! I'm Thitipong Tapianthong, a student in network engineering at King Mongkut's University of Technology North Bangkok.
+            Hello there! I'm Thitipong Tapianthong, a student in network
+            engineering at King Mongkut's University of Technology North
+            Bangkok.
           </p>
 
           <p className="text-center md:text-left">
-            I'm interested in web development and programming. Mainly I use React, Next.js, and Tailwind CSS for my projects.
+            I'm interested in web development and programming. Mainly I use
+            React, Next.js, and Tailwind CSS for my projects.
           </p>
         </div>
 
@@ -39,16 +44,14 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col gap-4">
-        <h2 className="text-2xl font-bold">
-          My Projects
-        </h2>
+        <h2 className="text-2xl font-bold">My Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          { 
-            projects
-              .filter(({ publish }) => publish)
-              .sort((a, b) => sortByDate(b.date, a.date))
-              .map((project, index) => <Project key={index} project={project} />)
-          }
+          {projects
+            .filter(({ publish }) => publish)
+            .sort((a, b) => sortByDate(b.date, a.date))
+            .map((project, index) => (
+              <Project key={index} project={project} />
+            ))}
         </div>
       </div>
 
