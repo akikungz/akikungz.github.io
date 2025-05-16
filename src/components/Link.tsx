68881f2372aaff6a805e1ec424dde410cb2140dc
Thitipong Tapianthong <akikungz@outlook.com>
2025-05-16 22:54:11 +0700

    Update Link.tsx

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -8,7 +8,7 @@ export default function Link() {
   const link = searchParams.get("link");
 
   if (link) {
-    const url?: Record<string, string> = (target as Record<string, unknown>)[link];
+    const url: Record<string, string> = (target as Record<string, unknown>)[link];
 
     if (url) {
       window.location.href = url[href];
